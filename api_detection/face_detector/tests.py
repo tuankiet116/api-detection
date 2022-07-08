57a55b02529eb1e50a1d040947b8893b4f5ee5cf
kiet.do <kiet.do@pirago.work>
2022-07-08 17:34:10 +0700

    detection

--- api_detection/face_detector/tests.py
+++ api_detection/face_detector/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+import requests
+import cv2
 
-# Create your tests here.
+# define the URL to our face detection API
+url = "http://127.0.0.1:8002/face_detection/detect"
+# use our face detection API to find faces in images via image URL
+image = cv2.imread("obama.jpg")
+payload = {"url": "https://pyimagesearch.com/wp-content/uploads/2015/05/obama.jpg"}
+r = requests.post(url, data=payload).json()
+print("obama.jpg: {}".format(r))
+# loop over the faces and draw them on the image
+for (startX, startY, endX, endY) in r["faces"]:
+    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+# show the output image
+cv2.imshow("obama.jpg", image)
+cv2.waitKey(0)
+# load our image and now use the face detection API to find faces in
+# images by uploading an image directly
+image = cv2.imread("adrian.jpg")
+payload = {"image": open("adrian.jpg", "rb")}
+r = requests.post(url, files=payload).json()
+print("adrian.jpg: {}".format(r))
+# loop over the faces and draw them on the image
+for (startX, startY, endX, endY) in r["faces"]:
+    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+# show the output image
+cv2.imshow("adrian.jpg", image)
+cv2.waitKey(0)
--- api_detection/face_detector/views.py
+++ api_detection/face_detector/views.py
@@ -2,7 +2,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import numpy as np
-import urllib
+import urllib.request as urllib
 import json
 import cv2
 import os
@@ -13,30 +13,22 @@ FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".
 
 @csrf_exempt
 def detect(request):
-    # initialize the data dictionary to be returned by the request
     data = {"success": False}
-    # check to see if this is a post request
     if request.method == "POST":
-        # check to see if an image was uploaded
         if request.FILES.get("image", None) is not None:
-            # grab the uploaded image
             image = _grab_image(stream=request.FILES["image"])
-        # otherwise, assume that a URL was passed in
         else:
-            # grab the URL from the request
             url = request.POST.get("url", None)
-            # if the URL is None, then return an error
             if url is None:
                 data["error"] = "No URL provided."
                 return JsonResponse(data)
-            # load the image and convert
             image = _grab_image(url=url)
         # convert the image to grayscale, load the face cascade detector,
         # and detect faces in the image
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
         rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
-                                          minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+                                          minSize=(30, 30))
         # construct a list of bounding boxes from the detection
         rects = [(int(x), int(y), int(x + w), int(y + h))
                  for (x, y, w, h) in rects]
@@ -47,10 +39,8 @@ def detect(request):
 
 
 def _grab_image(path=None, stream=None, url=None):
-    # if the path is not None, then load the image from disk
     if path is not None:
         image = cv2.imread(path)
-    # otherwise, the image does not reside on disk
     else:
         # if the URL is not None, then download the image
         if url is not None:
@@ -63,6 +53,4 @@ def _grab_image(path=None, stream=None, url=None):
         # OpenCV format
         image = np.asarray(bytearray(data), dtype="uint8")
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-
-    # return the image
     return image
