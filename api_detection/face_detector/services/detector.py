8df418353f0f90931695ca7f6f4fcf0a639de5dc
kiet.do <kiet.do@pirago.work>
2022-07-12 17:18:16 +0700

    convention

---
+++ api_detection/face_detector/services/detector.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+import cv2
+
+FACE_DETECTOR_PATH = "{base_path}/../models/haarcascade_frontalface_default.xml".format(
+    base_path=os.path.abspath(os.path.dirname(__file__)))
+FACE_DETECTOR_DLIB_PATH = "{base_path}/../models/mmod_human_face_detector.dat".format(
+    base_path=os.path.abspath(os.path.dirname(__file__)))
+
+def haarcascade_frontalface_default(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
+    rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    rects = [(int(x), int(y), int(x + w), int(y + h))
+             for (x, y, w, h) in rects]
+    return rects
+
+def dlib(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    detector = dlib.cnn_face_detection_model_v1(FACE_DETECTOR_DLIB_PATH)
+    result = detector(image)
+    rects = [(int(x), int(y), int(x + w), int(y + h))
+             for (x, y, w, h) in result]
+    return rects
+
+def convertFromBytes(data):
+    image = np.asarray(bytearray(data), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
--- api_detection/face_detector/views.py
+++ api_detection/face_detector/views.py
@@ -1,38 +1,30 @@
 # import the necessary packages
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
-import numpy as np
 import urllib.request as urllib
-import json
-import cv2
-import os
-
-# import pdb; pdb.set_trace()
-# define the path to the face detector
-FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
-    base_path=os.path.abspath(os.path.dirname(__file__)))
-
+from .services.detector import haarcascade_frontalface_default, dlib, convertFromBytes
 
 @csrf_exempt
 def detect(request):
-    data = {"success": False}
+    response = {"success": False}
     if request.method == "POST":
         if request.FILES.get("image", None) is not None:
-                image = _grab_image(stream=request.FILES.get("image"))
+                data_image = _grab_image(stream=request.FILES.get("image"))
         else:
             url = request.POST.get("url", None)
             if url is None:
-                data["error"] = "No URL provided."
-                return JsonResponse(data)
-            image = _grab_image(url=url)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
-        rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
-                                            minSize=(30, 30))
-        rects = [(int(x), int(y), int(x + w), int(y + h))
-                    for (x, y, w, h) in rects]
-        data.update({"num_faces": len(rects), "faces": rects, "success": True})
-    return JsonResponse(data)
+                response["error"] = "No URL provided."
+                return JsonResponse(response)
+            data_image = _grab_image(url=url)
+        
+        mod = request.POST.get('algorithm', 'dlib')
+        data_image = convertFromBytes(data_image)
+        if mod == 'dlib':
+            rects = dlib(data_image)
+        else:
+            rects = haarcascade_frontalface_default(data_image)
+        response.update({"num_faces": len(rects), "faces": rects, "success": True})
+    return JsonResponse(response)
 
 
 def _grab_image(stream=None, url=None):
@@ -41,6 +33,4 @@ def _grab_image(stream=None, url=None):
         data = resp.read()
     elif stream is not None:
         data = stream.read()
-    image = np.asarray(bytearray(data), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    return image
+    return data
